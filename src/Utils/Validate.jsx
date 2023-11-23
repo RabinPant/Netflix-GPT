9a2082ba7ce85b42b06736052f283b05ff109235
= <=>
2023-11-23 09:57:00 -0500

    bug fix

---
+++ src/Utils/Validate.jsx
@@ -0,0 +1,16 @@
+export const checkValidData = (email, password) => {
+  //   const isNameValid =
+  //     /^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/.test(name);
+
+  const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
+    email
+  );
+  const isPasswordValid =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+
+  //   if (!isNameValid) return "Name is not Valid";
+  if (!isEmailValid) return "Email ID is not valid";
+  if (!isPasswordValid) return "Password is not valid";
+
+  return null;
+};
